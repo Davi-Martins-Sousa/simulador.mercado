de3153c9ac87137448e8124177d43a73e0850592
Davi-Martins-Sousa <davi470063@gmail.com>
2023-09-06 12:16:02 -0300

    media movel

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def create(base):
 def start(data):
     print(f'Início do dia: {data}')
 
-def update(capital, posicao, precoOntem, precoHoje, primeiroUltimoDia):
+def updateBase(capital, posicao, precoOntem, precoHoje, primeiroUltimoDia):
     if precoOntem is not None and primeiroUltimoDia is False:
         if precoHoje > precoOntem and posicao > -1:
             capital, posicao, precoHoje = venda(capital, posicao, precoHoje, 2)
@@ -27,6 +27,19 @@ def update(capital, posicao, precoOntem, precoHoje, primeiroUltimoDia):
 
     return capital, posicao, precoHoje
 
+def updateMeidaMovel(capital,posicao,precoHoje,mediaLonga,mediaCurta,primeiroUltimoDia):
+    if primeiroUltimoDia is False and mediaLonga != None:
+        if mediaLonga >= mediaCurta and posicao > -1:
+            capital, posicao, precoHoje = venda(capital, posicao, precoHoje, 2)
+        elif mediaLonga <= mediaCurta and posicao < 1:
+            capital, posicao, precoHoje = compra(capital, posicao, precoHoje, 2)
+    elif primeiroUltimoDia is True  and mediaLonga != None:
+        if mediaLonga >= mediaCurta: 
+            capital, posicao, precoHoje = venda(capital, posicao, precoHoje, 1)
+        elif mediaLonga <= mediaCurta:
+            capital, posicao, precoHoje = compra(capital, posicao, precoHoje, 1)
+    return capital, posicao
+
 def compra(capital, posicao, precoHoje, volume):
     posicao += volume
     capital += volume * precoHoje - 0.00  # preço de hoje - slippage
@@ -36,6 +49,10 @@ def venda(capital, posicao, precoHoje, volume):
     posicao -= volume
     capital -= volume * precoHoje + 0.00  # preço de hoje + slippage
     return capital, posicao, precoHoje
+
+def media_movel(data, periodo):
+    media_movel = data['Close'].rolling(window=periodo).mean()
+    return media_movel
 
 def finish(data):
     print(f'Fim do dia: {data}')
@@ -47,11 +64,15 @@ def main():
     fechamentos = []
     riqueza = []
     base = create('dados/BBDC4.csv')
+    periodo = 10
+    base['MediaMovelLonga'] = media_movel(base, 20)
+    base['MediaMovelCurta'] = media_movel(base, 7)
 
     
     for indice, hoje in base.iterrows():
         #start(hoje['Date'])
-        capital, posicao, precoOntem = update(capital, posicao, precoOntem, hoje['Close'],base.index[-1] == indice or base.index[1] == indice)
+        #capital, posicao, precoOntem = updateBase(capital, posicao, precoOntem, hoje['Close'],base.index[-1] == indice or base.index[1] == indice)
+        capital, posicao = updateMeidaMovel(capital, posicao, hoje['Close'],hoje['MediaMovelLonga'],hoje['MediaMovelCurta'],base.index[-1] == indice or base.index[1] == indice)
         riqueza_atual = capital - posicao * hoje["Close"]
         fechamentos.append(hoje["Close"])
         riqueza.append(float(riqueza_atual))
