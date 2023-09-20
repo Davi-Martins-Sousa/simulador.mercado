7a0af21b7eaf25a00e563bcf5a3446ce52c342ef
Davi-Martins-Sousa <davi470063@gmail.com>
2023-09-20 11:04:13 -0300

    ia-v2

--- estrategioAprendisadoSupervisionado.py
+++ estrategioAprendisadoSupervisionado.py
@@ -1,7 +1,5 @@
 from estrategia import create,start,finish,compra,venda
 
-
-
 def aprendizadoSupervisionado(base):
     capital = 0
     posicao = 0
@@ -10,13 +8,15 @@ def aprendizadoSupervisionado(base):
 
     base = create(base)
     
-    base['Close_Amanha'] = base['Close'].shift(-1)
-    base['Retorno'] = base['Close_Amanha'] - base['Close']
-    base.drop('Close_Amanha', axis=1, inplace=True)
-    base.drop('Date', axis=1)
+    #base.drop('Date', axis=1, inplace= True)
+    base['Retorno'] =  base['Close'].shift(-1) - base['Close']
     base['alvo'] = base['Retorno'].apply(lambda retorno: 1 if retorno > 0 else 0)
-
-
+    base['SMA-8'] = base['Close'].rolling(window=8).mean()
+    base['SMA-32'] = base['Close'].rolling(window=32).mean()
+    base['EMA-8'] = base['Close'].ewm(span=8, adjust=False).mean()
+    base['EMA-32'] = base['Close'].ewm(span=32, adjust=False).mean()
+    
+    
     print(base)
 
 aprendizadoSupervisionado('./dados/BBDC4.csv')
